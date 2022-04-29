9a863de5088723eb2fc450dee8981883ec38cd2d
Asus ROG <asusr@outlook.com>
2022-04-29 04:48:01 +0100

    Website update includes projects

--- project/__init__.py
+++ project/__init__.py
@@ -2,9 +2,35 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+projects = [
+    {
+        "name": "Minbaingu website built with Wordpress",
+        "thumb": "img/minbaringu.png",
+        "hero": "img/minbaringu.png",
+        "categories": ["python", "web"],
+        "slug": "habit-tracking",
+        "prod": "https://udemy.com"
+    },
+    {
+        "name": "Broken Universe Comics website built with Wordpress",
+        "thumb": "img/broken-universe-comics.png",
+        "hero": "img/broken-universe-comics.png",
+        "categories": ["react", "javascript"],
+        "slug": "personal-finance"
+    },
+    {
+        "name": "Polton Inn Pub website built with Wordpress",
+        "thumb": "img/polton-inn-pub.png",
+        "hero": "img/polton-inn-pub.png",
+        "categories": ["writing"],
+        "slug": "api-docs"
+    }
+]
+
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", projects=projects)
 
 @app.route("/about")
 def about():
